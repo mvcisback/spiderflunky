20557056d86dd3cfa8656391a116418021e46183
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-06-11 12:49:30 -0700

    added the rest of the children tags

--- spiderflunky/js_ast.py
+++ spiderflunky/js_ast.py
@@ -41,9 +41,7 @@ class Node(dict):
                     yield depth+1, ret
 
     def _children(self):
-        # fail, there is no generic body attr.
-        raise NotImplementedError
-
+        return []
 
     def children(self):
         """Return my children, accounting for variations in where children are
@@ -90,117 +88,6 @@ class Node(dict):
         return self['type']
 
 
-class VariableDeclaration(Node):
-    def _children(self):
-        return self['declarations']
-
-class EmptyStatement(Node):
-    def _children(self):
-        return None
-
-class ExpressionStatement(Node):
-    def _children(self):
-        return [self['expression']]
-
-class BlockStatement(Node):
-    def _children(self):
-        return self['body']
-
-class AssignmentExpression(Node):
-    def _children(self):
-        return [self['left'], self['right']]
-
-
-class IfStatement(Node):
-    def _children(self):
-        ret = [self['test'], self['consequent']]
-        if self['alternate']:
-            ret.append(self['alternate'])
-        return ret
-
-class LabeledStatement(Node):
-    def _children(self):
-        return [self['body']]
-
-class BreakStatement(Node):
-    def _children(self):
-        return None
-
-class ContinueStatement(Node):
-    def _children(self):
-        return None
-
-class WithStatement(Node):
-    def _children(self):
-        return [self['body'], self['object']]
-
-class SwitchStatement(Node):
-    def _children(self):
-        return [self['discriminant']] + self['cases'] + [self['lexical']]
-
-class ThrowStatement(Node):
-    def _children(self):
-        return [self['argument']]
-
-class ReturnStatement(Node):
-    def _children(self):
-        return [self['argument']]
-
-class TryStatement(Node):
-    def _children(self):
-        return [self['block'], self['handler']] + self['guardedHandlers'] + [self['finalizer']]
-
-class WhileStatement(Node):
-    def _children(self):
-        return [self['test'], self['body']]
-
-class DoWhileStatement(Node):
-    def _children(self):
-        return [self['body'], self['test']]
-
-class ForStatement(Node):
-    def _children(self):
-        return [self['init'], self['test'], self['update'], self['body']]
-
-class ForInStatement(Node):
-    def _children(self):
-        return [self['left'], self['right'],  self['body'], self['each']]
-
-class ForOfStatement(Node):
-    def _children(self):
-        return [self['left'], self['right'], self['body']]
-
-class Identifier(Node):
-    def _children(self):
-        return None
-
-class Literal(Node):
-    def _children(self):
-        return None
-
-
-
-class FunctionDeclaration(Node):
-    def scope(self):
-        """Return the set of symbols declared exactly at this node."""
-        # We store a set of symbols at each node that can hold a scope, except
-        # that we don't bother for the Program (global) scope. It holds
-        # everything we couldn't find elsewhere.
-
-        if '_scope' not in self:  # could store this in an instance var
-            # Find all the var decls within me, but don't go within any other
-            # functions. This implements hoisting.
-            self['_scope'] = set(
-                node['id']['name'] for _, node in self.walk_down(
-                    skip=lambda n: n['type'] == 'FunctionDeclaration')
-                if node['type'] == 'VariableDeclarator') | \
-                set(param['name'] for param in self['params'])
-        return self['_scope']
-
-    def _children(self):
-        raise NotImplementedError(self['type'])
-
-
 class Program(Node):
     """A Reflect.parse AST with some other handy properties
 
@@ -241,9 +128,308 @@ class Program(Node):
         return self['body']
 
 
+class Function(Node):
+    def _children(self):
+        return [self['id']] + self['params'] + self['defaults'] +\
+            [self['rest'], self['body'], self['generator'], self['expression']]
+
+# Statements
+
+class EmptyStatement(Node):
+    pass
+
+class BlockStatement(Node):
+    def _children(self):
+        return self['body']
+
+class ExpressionStatement(Node):
+    def _children(self):
+        return [self['expression']]
+
+class IfStatement(Node):
+    def _children(self):
+        ret = [self['test'], self['consequent']]
+        if self['alternate']:
+            ret.append(self['alternate'])
+        return ret
+
+class LabeledStatement(Node):
+    def _children(self):
+        return [self['body']]
+
+class BreakStatement(Node):
+    pass
+
+class ContinueStatement(Node):
+    def _children(self):
+        return [self['object'], self['body']]
+
+class WithStatement(Node):
+    def _children(self):
+        return [self['body'], self['object']]
+
+class SwitchStatement(Node):
+    def _children(self):
+        return [self['discriminant']] + self['cases'] + [self['lexical']]
+
+class ReturnStatement(Node):
+    def _children(self):
+        return [self['argument']]
+
+class ThrowStatement(Node):
+    def _children(self):
+        return [self['argument']]
+
+class TryStatement(Node):
+    def _children(self):
+        return [self['block'], self['handler']] + self['guardedHandlers'] + [self['finalizer']]
+
+class WhileStatement(Node):
+    def _children(self):
+        return [self['test'], self['body']]
+
+class DoWhileStatement(Node):
+    def _children(self):
+        return [self['body'], self['test']]
+
+class ForStatement(Node):
+    def _children(self):
+        return [self['init'], self['test'], self['update'], self['body']]
+
+class ForInStatement(Node):
+    def _children(self):
+        return [self['left'], self['right'], self['body'], self['each']]
+
+class ForOfStatement(Node):
+    def _children(self):
+        return [self['left'], self['right'], self['body']]
+
+class LetStatement(Node):
+    def _children(self):
+        return self['head'] + [self['body']]
+
+class DebuggerStatement(Node):
+    pass
+
+# Declarations
+
+class FunctionDeclaration(Node):
+    def scope(self):
+        """Return the set of symbols declared exactly at this node."""
+        # We store a set of symbols at each node that can hold a scope, except
+        # that we don't bother for the Program (global) scope. It holds
+        # everything we couldn't find elsewhere.
+
+        if '_scope' not in self:  # could store this in an instance var
+            # Find all the var decls within me, but don't go within any other
+            # functions. This implements hoisting.
+            self['_scope'] = set(
+                node['id']['name'] for _, node in self.walk_down(
+                    skip=lambda n: n['type'] == 'FunctionDeclaration')
+                if node['type'] == 'VariableDeclarator') | \
+                set(param['name'] for param in self['params'])
+        return self['_scope']
+
+    def _children(self):
+        raise NotImplementedError(self['type'])
+
+class VariableDeclaration(Node):
+    def _children(self):
+        if 'declarations' in self:
+            return self['declarations'] + [self['kind']]
+        else:
+            return [self['id'], self['init']]
+
+# Expressions
+
+class ThisExpression(Node):
+    pass
+
+class ArrayExpression(Node):
+    def _children(self):
+        return self['elements']
+
+class ObjectExpression(Node):
+    def _children(self):
+        return self['properties']
+
+class FunctionExpression(Node):
+    def _children(self):
+        return [self['id']] + self['params'] + self['defaults'] + \
+            [self['rest'], self['body'], self['generator'], self['expression']]
+
+class ArrowExpression(Node):
+    def _children(self):
+        return self['params'] + self['defaults'] + \
+            [self['rest'], self['body'], self['generator'], self['expression']]
+
+class SequenceExpression(Node):
+    def _children(self):
+        return self['expressions']
+
+class UnaryExpression(Node):
+    def _children(self):
+        return [self['prefix'], self['argument']]
+
+class BinaryExpression(Node):
+    def _children(self):
+        return [self['left'], self['right']]
+
+class AssignmentExpression(Node):
+    def _children(self):
+        return [self['left'], self['right']]
+
+class UpdateExpression(Node):
+    def _children(self):
+        return [self['argument'], self['prefix']]
+
+class LogicalExpression(Node):
+    def _children(self):
+        return [self['left'], self['right']]
+
+class ConditionalExpression(Node):
+    def _children(self):
+        return [self['test'], self['alternate'], self['consequent']]
+
+class NewExpression(Node):
+    def _children(self):
+        return [self['callee']] + self['arguments']
+
+class CallExpression(Node):
+    def _children(self):
+        return [self['callee']] + self['arguments']
+
+class MemberExpression(Node):
+    def _children(self):
+        return [self['object'], self['property'], self['computed']]
+
+class YieldExpression(Node):
+    def _children(self):
+        return [self['argument']]
+
+class ComprehensionExpression(Node):
+    def _children(self):
+        return [self['body']] + self['blocks'] + [self['filter']]
+
+class GeneratorExpression(Node):
+    def _children(self):
+        return [self['body']] + self['blocks'] + [self['filter']]
+
+class GraphExpression(Node):
+    def _children(self):
+        return [self['index'], self['expression']]
+
+class GraphIndexExpression(Node):
+    def _children(self):
+        return [self['index']]
+
+class LetExpression(Node):
+    def _children(self):
+        return self['head'] + [self['body']]
+
+# Patterns
+
+class ObjectPattern(Node):
+    def _children(self):
+        return self['properties']
+
+class ArrayPattern(Node):
+    def _children(self):
+        return self['elements']
+
+# Clauses
+
+class SwitchCase(Node):
+    def _children(self):
+        return [self['test']] + self['consequent']
+
+class CatchClause(Node):
+    def _children(self):
+        return [self['param'], self['guard'], self['body']]
+
+class ComprehensionBlock(Node):
+    def _children(self):
+        return [self['left'], self['right'], self['each']]
+
+# Miscellaneous
+
+class Identifier(Node):
+    def _children(self):
+        return [self['name']]
+
+class Literal(Node):
+    pass
+
+# E4X
+class XMLDefaultDeclaration(Node):
+    def _children(self):
+        return [self['namespace']]
+
+class XMLAnyName(Node):
+    pass
+
+class XMLQualifiedIdentifier(Node):
+    def _children(self):
+        return [self['left'], self['right'], self['computed']]
+
+class XMLFunctionQualifiedIdentifier(Node):
+    def _children(self):
+        return [self['right'], self['computed']]
+
+class XMLFilterExpression(Node):
+    def _children(self):
+        return [self['left'], self['right']]
+
+class XMLElement(Node):
+    def _children(self):
+        return self['contents']
+
+class XMLList(Node):
+    def _children(self):
+        return self['contents']
+
+class XMLEscape(Node):
+    def _children(self):
+        return [self['expression']]
+
+class XMLText(Node):
+    def _children(self):
+        return [self['text']]
+
+class XMLStartTag(Node):
+    def _children(self):
+        return self['contents']
+
+class XMLEndTag(Node):
+    def _children(self):
+        return self['contents']
+
+class XMLPointTag(Node):
+    def _children(self):
+        if type(self['contents']) is list:
+            return self['contents']
+        else:
+            return [self['contents']]
+
+class XMLName(Node):
+    def _children(self):
+        return self['contents']
+
+class XMLComment(Node):
+    def _children(self):
+        return self['contents']
+
+class XMLProcessingInstruction(Node):
+    def _children(self):
+        return [self['target'], self['contents']]
+
+
 NODE_TYPES = {cls.__name__:cls for cls in globals().values() if
               isclass(cls) and issubclass(cls, dict)}
 
 def make_node(d):
     """Construct the right kind of Node for a raw Reflect.parse node."""
-    return NODE_TYPES.get(d.get('type'), Node)(d)
+    try:
+        return NODE_TYPES[d['type'], Node](d)
+    except KeyError:
+        raise NotImplementedError(d.get('type', "Unknown Type"))
